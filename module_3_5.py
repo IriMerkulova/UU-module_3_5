6104a1de405432c4b6a70910e537941d2ec9ad64
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-23 19:34:51 +0300

    Update module_3_5.py
    исправление если 0 и если много нолей

--- module_3_5.py
+++ module_3_5.py
@@ -1,36 +1,21 @@
 # Рекурсивное умножение
 
 def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
-
-    if first == 0:
-        for digit in str_number[1:]:
-            if digit != 0:
-                return get_multiplied_digits(int(digit + str_number[str_number.index(digit) + 1:]))
+    if number == 0: # это если о
         return 0
+    str_number = str(number)
+    if all(digit == '0' for digit in str_number): # если все нули
+        return 0
+    str_number = str_number.replace('0', '')  # удалили все '0' из строки
+    first = int(str_number[0])
     if len(str_number) > 1:
         return first*get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-result = get_multiplied_digits(40203)
-print(result)
-
-
-# ИЛИ МОЖНО ПРОСТО УБРАТЬ НОЛЬ:
-# def get_multiplied_digits(number):
-#     str_number = str(number)
-#     str_number = str_number.replace('0', '') # удалили все '0' из строки
-#     # print(str_number)
-#
-#     first = int(str_number[0])
-#
-#     if len(str_number) > 1:
-#         return first*get_multiplied_digits(int(str_number[1:]))
-#     else:
-#         return first
-#
-#
-# result = get_multiplied_digits(402030)
-# print(result)
+result1 = get_multiplied_digits(402030)
+result2 = get_multiplied_digits(00000)
+result3 = get_multiplied_digits(0)
+print(result1)
+print(result2)
+print(result3)
